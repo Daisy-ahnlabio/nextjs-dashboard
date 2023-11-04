7c9ab44d0d38dc913c7632b3261dada8ef3ddb7e
Daisy-ahnlabio <daisy@ahnlab.io>
2023-11-05 04:45:23 +0900

    feat // ~14 장

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -1,17 +1,15 @@
-import Image from 'next/image';
-import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
-import InvoiceStatus from '@/app/ui/invoices/status';
-import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
-import { fetchFilteredInvoices } from '@/app/lib/data';
+import Image from "next/image";
+import { UpdateInvoice, DeleteInvoice } from "@/app/ui/invoices/buttons";
+import InvoiceStatus from "@/app/ui/invoices/status";
+import { formatDateToLocal, formatCurrency } from "@/app/lib/utils";
+import { fetchFilteredInvoices } from "@/app/lib/data";
 
 export default async function InvoicesTable({
-  query,
-  currentPage,
+  invoices,
 }: {
-  query: string;
-  currentPage: number;
+  invoices: InvoiceTable[];
 }) {
-  const invoices = await fetchFilteredInvoices(query, currentPage);
+  // const invoices = await fetchFilteredInvoices(query, currentPage);
 
   return (
     <div className="mt-6 flow-root">
