abad71e3a157227d8520bc55bf74cbdb2fd30375
Daisy <daisy@ahnlab.io>
2023-11-09 18:59:32 +0900

    feat // 인증추가

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -12,6 +12,13 @@ import {
   LatestInvoicesSkeleton,
   CardsSkeleton,
 } from "@/app/ui/skeletons";
+import { lusitana } from "@/app/ui/fonts";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
 
 export default async function Page() {
   const revenue = await fetchRevenue();
@@ -25,7 +32,9 @@ export default async function Page() {
 
   return (
     <main>
-      <h1 className={` mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
